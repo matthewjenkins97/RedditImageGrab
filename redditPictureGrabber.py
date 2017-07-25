61610809b6cf116aedc9ccc0418d0e4b4a6b65bd
Matthew R. Jenkins <matthewjenkins97@gmail.com>
2017-07-24 22:49:42 -0400

    the mac version should work now.

--- redditPictureGrabber.py
+++ redditPictureGrabber.py
@@ -5,6 +5,7 @@ import time
 import shutil
 import subprocess
 
+
 def get_path():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
 
@@ -14,22 +15,23 @@ def win_picture_grabber(destination_path, subreddit):
 
 
 def mac_picture_grabber(destination_path, subreddit):
-    myfile = open('iteration', 'r+')
-    numStr = myfile.read()
-    num = int(numStr)
+    """the mac version of the picture downloader."""
+    myfile = open('iteration', 'w+')
+    myfile.write("1")
+    num = 1
     while True:
         myfile.close()
         print("Current iteration is: " + str(num))
         # if num is 0, delete everything inside folder
         if num == 0:
             shutil.rmtree(destination_path)
-
-        subprocess.Popen(["python", get_path() + "/redditdl.py", "--sort-type today", "--num 1", "update", "--sfw", "--skipAlbums", subreddit, destination_path])
+        subprocess.Popen(["python", get_path() + "/redditdl.py", subreddit,
+                         destination_path, "--sort-type", "topday", "--num",
+                         "1", "--skipAlbums"])
         time.sleep(1740)
-
         # incrementing and modulo
-        num = num + 1
-        num = num % 48
+        num += 1
+        num %= 48
         myfile = open('iteration', 'r+')
         myfile.write(str(num))
 
@@ -41,7 +43,6 @@ def main():
         # dump argv into variables
         destination_path = sys.argv[1]
         subreddit = sys.argv[2]
-
         # checks if destination_path exists. if it does not, it quits.
         if os.path.exists(destination_path):
             # checking what OS is running and execute a function based off the
